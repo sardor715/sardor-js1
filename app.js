ae0c4915675631018f6ec76a7b7af9faa61404a8
sardor <76@ru>
2023-09-16 13:27:29 +0500

    sardor

---
+++ app.js
@@ -0,0 +1,42 @@
+//первое-задание
+
+var imya = prompt('Введите свое имя')
+
+console.log('Меня' + ' зовут ' + imya);
+
+var age = prompt('Ввдедите свой возвраст')
+
+console.log('Мне ' + age + ' лет');
+
+var plus  = prompt('Решите пример 10 + 6 = ?')
+
+console.log('Пример 1: 10 + 6 = ' + (10 + 6) + ' Ваш ответ ' + plus);
+
+var  minus = prompt('Решите пример 34 - 9 = ?')
+
+console.log('Пример 2: 34 - 9 = ' + (34 - 9)  + ' Ваш ответ ' +  minus);
+
+var  multiplication = prompt('Решите пример 5 * 7 = ?' )
+
+console.log('Пример 3: 5 * 7 = ' + 5 * 7 + ' Ваш ответ ' + multiplication);
+
+var devide = prompt('Решите пример 32 / 4 = ?')
+
+console.log( 'Пример 4: 32 / 4 = ' + 32 / 4 + ' Ваш ответ ' + devide );
+
+var procent = prompt('Решите пример 27 % 5 = ?')
+
+console.log('Пример 5: 27 % 5 = ' + 27 % 5 + ' Ваш ответ ' + procent);
+ 
+ alert('Откройте консоль для просмотра ');
+ 
+ 
+
+//второе-задание
+
+var X = +prompt('Введите первое число')
+var Y = +prompt('Введите второе число')
+var Z = +prompt('Введите третье число')
+
+
+alert('Среднее арифметическое:' + (X + Y + Z)  / 3)
